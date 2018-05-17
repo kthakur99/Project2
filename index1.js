6c069177f7b4920208fe96d2d391b042e3b5e09f
Kunal Thakur <KunalThakur@kunals-mbp-2.coldspring.marian.edu>
2018-05-17 15:10:18 -0400

    index.js rednerList update

--- index1.js
+++ index1.js
@@ -1,10 +1,35 @@
 const app = {
- init: function() {
-   document
-    .querySelector('#flickForm')
-    .addEventListener()
+ init: function(selectors) {
+  this.max = 0
+
+  document
+    .querySelector(selectors.formSelector)
+    .addEventListener('submit', ev => {
+      ev.preventDefault()
+      this.handleSubmit(ev)
+    })
+ },
+
+ renderListItem: function() {
+   const item = document.createElement('li')
+   item.textContent = flick.name
+   return item
+ },
+
+ handleSubmit: function(ev){
+  ev.preventDefault()
+  const f = ev.target
+  const flick = {
+    id: ++this.max,
+    name: f.flickname.value,
+  }
+  console.log(flick)
+  f.reset()
  },
 }
 
 
-app.init()
+app.init({
+  formSelector: '#flickform',
+  listSelector: '#flicklist',
+})
