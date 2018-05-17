b629cfeaf092965d9001e8ee832bac6e180da65f
Kunal Thakur <KunalThakur@kunals-mbp-2.coldspring.marian.edu>
2018-05-17 15:25:02 -0400

    add flix to the beginning

--- index1.js
+++ index1.js
@@ -1,35 +1,47 @@
 const app = {
- init: function(selectors) {
-  this.max = 0
+  init(selectors) {
+    this.flicks = []
+    this.max = 0
+    this.list = document.querySelector(selectors.listSelector)
+    this.template = document.querySelector(selectors.templateSelector)
 
-  document
-    .querySelector(selectors.formSelector)
-    .addEventListener('submit', ev => {
-      ev.preventDefault()
-      this.handleSubmit(ev)
-    })
- },
+    document
+      .querySelector(selectors.formSelector)
+      .addEventListener('submit', ev => {
+        ev.preventDefault()
+        this.handleSubmit(ev)
+      })
+  },
 
- renderListItem: function() {
-   const item = document.createElement('li')
-   item.textContent = flick.name
-   return item
- },
+  renderListItem(flick) {
+    const item = this.template.cloneNode(true)
+    item.classList.remove('template')
+    item.dataset.id = flick.id
+    item
+      .querySelector('.flickName')
+      .textContent = flick.name
 
- handleSubmit: function(ev){
-  ev.preventDefault()
-  const f = ev.target
-  const flick = {
-    id: ++this.max,
-    name: f.flickname.value,
-  }
-  console.log(flick)
-  f.reset()
- },
+    return item
+  },
+
+  handleSubmit(ev) {
+    const f = ev.target
+    const flick = {
+      id: ++this.max,
+      name: f.flickName.value,
+    }
+
+    this.flicks.unshift(flick)
+
+    const item = this.renderListItem(flick)
+    this.list.insertBefore(item, this.list.firstElementChild)
+
+    f.reset()
+  },
 }
 
-
 app.init({
-  formSelector: '#flickform',
-  listSelector: '#flicklist',
+  formSelector: '#flickForm',
+  listSelector: '#flickList',
+  templateSelector: '.flick.template',
 })
